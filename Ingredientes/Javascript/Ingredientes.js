27d4876e46ab307fb5f931e70d705c15ba681420
Juan Lu <j_l_juan@hotmail.com>
2017-01-10 16:50:43 +0100

    Añadiendo ejercicio
    Subida de ejercicio para obtener el json

---
+++ Ingredientes/Javascript/Ingredientes.js
@@ -0,0 +1,25 @@
+﻿/*
+	Descripción de la práctica
+	• Escribir un JSon de ingredientes
+		· chocolate: harina, azucar, mantequilla, cacao
+*/
+var header = document.querySelector("header");
+var section = document.querySelector("section");
+
+var requestURL = "../JSon/ingredientes.json";
+var request = new XMLHttpRequest();
+request.open("GET", requestURL);
+request.responseType = 'json';
+request.send();
+
+
+//-------------
+
+request.onload = function() {
+	var ingredientes = request.response;
+	verIngredientes(ingredientes);
+}
+
+function verIngredientes(varJSon){
+	alert("Prueba: "+ varJSon['squadName']);
+}
